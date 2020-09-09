5a8dd03f4c8b52bf84745c98ac3bdab6d605ce66
DevArt <52213952+DevArt002@users.noreply.github.com>
2020-09-10 04:28:40 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Camera from "./CameraPopup";
+
+import "./Dashboard.css";
+
+export default function App() {
+  return <Camera />;
+}
